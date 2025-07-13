9ce46cc6b990db2a1f1ea6ac6905c276619d8972
degudegu2510 <degudegu2510@gmail.com>
2025-07-13 23:35:37 +0900

    Fixed Toc

--- src/component/Molecules/TocList/TocList.tsx
+++ src/component/Molecules/TocList/TocList.tsx
@@ -11,8 +11,8 @@ export const TocList = ({
 }: Props) => {
   return (
     <ul aria-labelledby={ariaLabelledby}>
-      {items.map(item => (
-        <li key={item.id} className="grid grid-cols-1">
+      {items.map((item, index) => (
+        <li key={item.id + index} className="grid grid-cols-1">
           <a
             href={`#${item.id}`}
             className="py-2 pl-3 pr-2 body-1-dense border-l-1 border-gray rounded-r-sm hover:bg-surface-variant"
