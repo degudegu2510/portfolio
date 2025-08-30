f5f53c7c271ba9dd52711267a95a6c875728a38a
degudegu2510 <degudegu2510@gmail.com>
2025-08-30 11:36:33 +0900

    Add Articles Page Link

--- src/component/Organisms/Home/PostArticle.tsx
+++ src/component/Organisms/Home/PostArticle.tsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router"
 import { PostArticleList } from "../../Molecules/PostArticles/PostArticleList"
+import { MaterialSymbols } from "../../Atoms/MaterialSymbols/MaterialSymbols"
+import { ArticlesPagePath } from "../../../utils/Routes"
 
 export const PostArticle = () => {
 
@@ -6,6 +9,13 @@ export const PostArticle = () => {
     <section className="py-16">
       <h2 className="heading-1">投稿記事</h2>
       <PostArticleList />
+      <Link
+        to={ArticlesPagePath()}
+        className="flex items-center hover:bg-surface p-2 pr-0.5 rounded mt-6 w-fit"
+      >
+        もっと見る
+        <MaterialSymbols size={24}>chevron_right</MaterialSymbols>
+      </Link>
     </section>
   )
 }
