6a73903368f4dbf926f1e3334b827fdd61dd811c
degudegu2510 <degudegu2510@gmail.com>
2025-12-30 02:00:43 +0900

    Fixed pagenation

--- src/component/Molecules/Pagination/Pagenation.tsx
+++ src/component/Molecules/Pagination/Pagenation.tsx
@@ -1,4 +1,5 @@
 import { MaterialSymbols } from "../../Atoms/MaterialSymbols/MaterialSymbols";
+import { Link } from "react-router";
 
 interface PaginationProps {
   totalPage: number;
@@ -29,44 +30,87 @@ export const Pagination = ({ totalPage, currentPage, href, className }: Paginati
 
   return (
     <nav aria-label="ページネーション" className={`flex justify-center ${className}`}>
-      <a
-        href={addParams(1)}
-        aria-label="最初のページに戻る"
-        className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
-      >
-        <MaterialSymbols>keyboard_double_arrow_left</MaterialSymbols>
-      </a>
-      <a
-        href={addParams(currentPage - 1)}
-        aria-label="前のページに戻る"
-        className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
-      >
-        <MaterialSymbols>chevron_left</MaterialSymbols>
-      </a>
+      {currentPage === 1 ? (
+        <>
+          <span
+            aria-disabled
+            aria-label="最初のページに戻る"
+            className="flex items-center justify-center px-1 py-1 rounded-lg text-medium-emphasis"
+          >
+            <MaterialSymbols>keyboard_double_arrow_left</MaterialSymbols>
+          </span>
+          <span
+            aria-disabled
+            aria-label="前のページに戻る"
+            className="flex items-center justify-center px-1 py-1 rounded-lg text-medium-emphasis"
+          >
+            <MaterialSymbols>chevron_left</MaterialSymbols>
+          </span>
+        </>
+      ) : (
+        <>
+          <Link
+            to={addParams(1)}
+            aria-label="最初のページに戻る"
+            className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
+          >
+            <MaterialSymbols>keyboard_double_arrow_left</MaterialSymbols>
+          </Link>
+          <Link
+            to={addParams(currentPage - 1)}
+            aria-label="前のページに戻る"
+            className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
+          >
+            <MaterialSymbols>chevron_left</MaterialSymbols>
+          </Link>
+        </>
+      )}
+
       {pageNumbers.map((num) => (
-        <a
+        <Link
           key={num}
-          href={addParams(num)}
+          to={addParams(num)}
           aria-current={num === currentPage ? "page" : undefined}
           className={`body-2-bold flex items-center justify-center px-1 py-1 rounded-lg min-w-8 ${currentPage == num ? 'text-on-container bg-gray hover:bg-gray-dim' : 'hover:bg-surface-variant'}`}
         >
           {num}
-        </a>
+        </Link>
       ))}
-      <a
-        href={addParams(currentPage + 1)}
-        aria-label="次のページに進む"
-        className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
-      >
-        <MaterialSymbols>chevron_right</MaterialSymbols>
-      </a>
-      <a
-        href={addParams(totalPage)}
-        aria-label="最後のページに進む"
-        className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
-      >
-        <MaterialSymbols>keyboard_double_arrow_right</MaterialSymbols>
-      </a>
+      {totalPage === currentPage ? (
+        <>
+          <span
+            aria-disabled
+            aria-label="次のページに進む"
+            className="flex items-center justify-center px-1 py-1 rounded-lg text-medium-emphasis"
+          >
+            <MaterialSymbols>chevron_right</MaterialSymbols>
+          </span>
+          <span
+            aria-disabled
+            aria-label="最後のページに進む"
+            className="flex items-center justify-center px-1 py-1 rounded-lg text-medium-emphasis"
+          >
+            <MaterialSymbols>keyboard_double_arrow_right</MaterialSymbols>
+          </span>
+        </>
+      ):(
+        <>
+          <Link
+            to={addParams(currentPage + 1)}
+            aria-label="次のページに進む"
+            className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
+          >
+            <MaterialSymbols>chevron_right</MaterialSymbols>
+          </Link>
+          <Link
+            to={addParams(totalPage)}
+            aria-label="最後のページに進む"
+            className="flex items-center justify-center px-1 py-1 rounded-lg hover:bg-surface-variant"
+          >
+            <MaterialSymbols>keyboard_double_arrow_right</MaterialSymbols>
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
