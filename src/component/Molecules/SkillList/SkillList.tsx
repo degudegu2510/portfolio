1438ccba57df513af662d9e7a33712c90ace8444
degudegu2510 <degudegu2510@gmail.com>
2025-12-30 02:02:29 +0900

    Fixed skill list

--- src/component/Molecules/SkillList/SkillList.tsx
+++ src/component/Molecules/SkillList/SkillList.tsx
@@ -9,11 +9,11 @@ export const SkillList = ({ className }: SkillListProps) => {
   return (
     <ul className={`grid grid-cols-[minmax(0,_1fr)] gap-2 ${className}`}>
       {skillsList.map( skillList => (
-        <li className="backdrop-blur-[2px] backdrop-filter bg-surface-alpha rounded-lg border border-gray-variant p-[8px_16px]">
+        <li key={skillList.title} className="backdrop-blur-[2px] backdrop-filter bg-surface-alpha rounded-lg border border-gray-variant p-[8px_16px]">
           <p className="body-2-bold text-medium-emphasis">{skillList.title}</p>
           <ul className="flex flex-wrap gap-x-5 mt-1">
             {skillList.skills.map(skill => (
-              <li className="body-1-bold">{skill}</li>
+              <li key={skill} className="body-1-bold">{skill}</li>
             ))}
           </ul>
         </li>
